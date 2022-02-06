6e2b33974b1603781660e639d74c1c8a89904315
charliedeus <charliedeus@gmail.com>
2022-02-06 18:37:46 -0300

    configure port to herokuapp

--- src/server.ts
+++ src/server.ts
@@ -34,4 +34,6 @@ app.use(
   },
 );
 
-app.listen(4000, () => console.log(`🚀 Server is running on PORT 4000!!!`));
+app.listen(process.env.PORT || 4000, () =>
+  console.log(`🚀 Server is running on PORT 4000!!!`),
+);
